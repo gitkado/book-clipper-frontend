768cacf3d2e8d64cf35067c29a9eb07f7339e3a7
gitkado <kd.311440@gmail.com>
2019-12-28 16:13:11 +0000

    201912 dev

--- book-clipper/pages/books/index.vue
+++ book-clipper/pages/books/index.vue
@@ -78,7 +78,7 @@
     <v-hover>
       <template v-slot="{ hover }">
         <nuxt-link v-bind:to="add.to" tag="span">
-          <v-btn class="ma-2" fab dark color="indigo" :elevation="hover ? 8 : 2">
+          <v-btn class="ma-2" fab dark small color="indigo" :elevation="hover ? 8 : 2">
             <v-icon dark>mdi-plus</v-icon>
           </v-btn>
         </nuxt-link>
